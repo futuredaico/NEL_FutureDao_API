7484d601a64b1e37c0a02d29efff34f7550cb700
tssqzr <1214661466@qq.com>
2019-09-05 17:41:01 +0800

    新增应用类型

--- Controllers/ShareController.cs
+++ Controllers/ShareController.cs
@@ -3,27 +3,40 @@ using NEL.Comm;
 using NEL.NNS.lib;
 using NEL_FutureDao_API.lib;
 using Newtonsoft.Json.Linq;
+using System;
 
 namespace NEL_FutureDao_API.Controllers
 {
     [Route("api/[controller]")]
     public class ShareController : Controller
     {
-        [HttpGet("{projId}/{net}"),HttpPost("{projId}/{net}")]
-        public void Get(string projId, int net)
+        [HttpGet("{projId}/{net}/{type}")]
+        public void Get(string projId, int net, int type)
         {
-            //return GetShareReturnHtml(projId, net);
-            var res = GetShareReturnHtml(projId, net);
-            Response.ContentType = "text/html; charset=UTF-8";
-            Response.Body.Write(System.Text.Encoding.UTF8.GetBytes(res));
-            Response.Body.Flush();
+            try
+            {
+                var res = GetShareReturnHtml(projId, net, type); ;
+                Response.ContentType = "text/html; charset=UTF-8";
+                Response.Body.Write(res.toBytes());
+                Response.Body.Flush();
+            } catch (Exception ex)
+            {
+                string errMsg = "Internal error:" + ex.Message;
+                Response.Body.Write(errMsg.toBytes());
+                Response.Body.Flush();
+            }
         }
 
-        private string GetShareReturnHtml(string projId, int net)
+        private string GetShareReturnHtml(string projId, int net, int type)
         {
-            var callback = getCallback(projId, net);
             getProjInfo(projId, net, out string projBrief, out string projConverUrl);
-            return string.Format(shareReturnHtmlFmt, "summary_large_image", "FutureDao", projBrief, projConverUrl, callback);
+            var callback = getCallback(projId, net);
+            //
+            if (type == 1)
+            {
+                return string.Format(shareReturnHtmlFmtOg, "summary_large_image", "FutureDao", projBrief, projConverUrl, callback);
+            }
+            return string.Format(shareReturnHtmlFmt, "website", "FutureDao", projBrief, projConverUrl, callback);
 
         }
         private void getProjInfo(string projId, int net, out string projBrief, out string projConverUrl)
@@ -57,6 +70,7 @@ namespace NEL_FutureDao_API.Controllers
             if (net == 1) callback = mainnetShareReturnCallbackUrlPrefix;
             return callback + projId;
         }
+        
         private string shareReturnHtmlFmt = ""
             + "<!DOCTYPE html>"
             + "\r\n<html lang=\"en\">"
@@ -64,7 +78,23 @@ namespace NEL_FutureDao_API.Controllers
             + "\r\n<meta data-rh=\"true\" name=\"twitter:card\" content=\"{0}\" />"
             + "\r\n<meta data-rh=\"true\" name=\"twitter:title\" content=\"{1}\" />"
             + "\r\n<meta data-rh=\"true\" name=\"twitter:description\" content=\"{2}\" />"
-            + "\r\n<meta data-rh=\"true\" name=\"twitter:image.src\" content=\"{3}\" />"
+            + "\r\n<meta data-rh=\"true\" name=\"twitter:image\" content=\"{3}\" />"
+            + "\r\n</head>"
+            + "\r\n<script>"
+            + "\r\nwindow.location.href = decodeURIComponent(decodeURI(\"{4}\"));"
+            + "\r\n</script>"
+            + "\r\n<body>"
+            + "\r\n</body>"
+            + "\r\n</html>"
+        ;
+        private string shareReturnHtmlFmtOg = ""
+            + "<!DOCTYPE html>"
+            + "\r\n<html lang=\"en\">"
+            + "\r\n<head>"
+            + "\r\n<meta data-rh=\"true\" property=\"og:type\" content=\"{0}\" />"
+            + "\r\n<meta data-rh=\"true\" property=\"og:title\" content=\"{1}\" />"
+            + "\r\n<meta data-rh=\"true\" property=\"og:description\" content=\"{2}\" />"
+            + "\r\n<meta data-rh=\"true\" property=\"og:image\" content=\"{3}\" />"
             + "\r\n</head>"
             + "\r\n<script>"
             + "\r\nwindow.location.href = decodeURIComponent(decodeURI(\"{4}\"));"
@@ -76,5 +106,12 @@ namespace NEL_FutureDao_API.Controllers
         private string testnetShareReturnCallbackUrlPrefix = "https://futuredao.nel.group/test/projectinfo/";
         private string mainnetShareReturnCallbackUrlPrefix = "https://futuredao.nel.group/projectinfo/";
 
+    }
+    static class ShareHelper
+    {
+        public static byte[] toBytes(this string data)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(data);
+        }
     }
 }
